e4260d16d35dfdb1be247b59b94f544e517760f9
Blackthrn <112625415+Blackthrn@users.noreply.github.com>
2023-02-19 17:35:40 +0000

    Can read in values

--- demeter_arduino_code/demeter_arduino_code.ino
+++ demeter_arduino_code/demeter_arduino_code.ino
@@ -1,11 +1,17 @@
 void setup() {
   
-  
+  Serial.begin(9600);
 
 }
 
 void loop() {
 
+  //Read in vlaues from sensors
+  int moistureValue = analogRead(A0);
+  int uvValue = analogRead(A1);
   
-
+  //Print Out Values
+  Serial.println(moistureValue);
+  delay(1000);
+  
 }
