3c1bb2d6552193da17cbeb7a01c65715af8f8adc
Blackthrn <112625415+Blackthrn@users.noreply.github.com>
2023-02-27 21:50:30 +0000

    Updating arduino code
    -Can print out data in serial
    -Started importing firebase library

--- demeter_arduino_code/demeter_arduino_code.ino
+++ demeter_arduino_code/demeter_arduino_code.ino
@@ -1,17 +1,32 @@
-void setup() {
-  
-  Serial.begin(9600);
+//Importing Libraries
+#include "Firebase_Arduino_WiFiNINA.h"
 
+//Pins
+int moisturePin = A0;
+int lightPin = A1;
+
+//Variables
+double currentMoisture = 0;
+double currentLight = 0;
+
+void setup() {
+
+  //Initialising Serial @ 9600 bits per second
+  Serial.begin(9600);
+  
 }
 
 void loop() {
 
-  //Read in vlaues from sensors
-  int moistureValue = analogRead(A0);
-  int uvValue = analogRead(A1);
+  //Fetching Data from Sensor
+  currentMoisture = analogRead(moisturePin);
+  currentLight = analogRead(lightPin);
+
+  //Writing Data
+  Serial.println(moisturePin);
+  Serial.println(lightPin);
   
-  //Print Out Values
-  Serial.println(moistureValue);
+  //Delaying Restart
   delay(1000);
   
 }
