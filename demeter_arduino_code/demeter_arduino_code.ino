445c2705296afac3865a1a15edd47c5ec1e21d15
Blackthrn <112625415+Blackthrn@users.noreply.github.com>
2023-02-28 16:14:06 +0000

    Finding a fix to why code not working
    -Changed internet to mobile hotspot
    -change input data type from int to double
    -Can now connect to wifi

--- demeter_arduino_code/demeter_arduino_code.ino
+++ demeter_arduino_code/demeter_arduino_code.ino
@@ -1,27 +1,51 @@
-//Importing Firebase Library
+//Including Firebase Library
 #include "Firebase_Arduino_WiFiNINA.h"
 #define DATABASE_URL "demeterdb-100d9-default-rtdb.europe-west1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 #define DATABASE_SECRET "Mbv7CFfRPZfRRVMn1OdOHTmjDpFLHxVyLtYKsHtD"
-#define WIFI_SSID "Vodafone-Guest"
-#define WIFI_PASSWORD "JBBDMX884K2D7EEK"
+#define WIFI_SSID "Alison's Phone"
+#define WIFI_PASSWORD "IWantADirtNap"
 
 //Other Libraries
 #include <WiFiNINA.h>
 
 //Pins
-int moisturePin = A0;
-int lightPin = A1;
+float moisturePin = A0;
+float lightPin = A1;
 
 //Variables
 double currentMoisture = 0;
 double currentLight = 0;
 
+String moisturePath = "/Current Data/Moisture";
+String lightPath = "/Current Data/UV";
+
+const String currentDataPath = "/Current Data";
+
 FirebaseData fbdo;
 
+//Arduino Setup
 void setup() {
 
   //Initialising Serial @ 9600 bits per second
   Serial.begin(9600);
+
+  //Letting User Know arduino is being set up
+  delay(2000);
+  Serial.println("Setting up arduino... Please wait a moment");
+
+  //Trying to connect to internet
+  Serial.print("Connecting to WiFi...");
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED)
+  {
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print(".");
+    delay(300);
+  }
+
+  //Providing Authentification Data for Firebase
+  Firebase.begin(DATABASE_URL, DATABASE_SECRET, WIFI_SSID, WIFI_PASSWORD);
+  Firebase.reconnectWiFi(true);
   
 }
 
@@ -31,9 +55,13 @@ void loop() {
   currentMoisture = analogRead(moisturePin);
   currentLight = analogRead(lightPin);
 
-  //Writing Data
-  Serial.println(moisturePin);
-  Serial.println(lightPin);
+  //Writing Data in Serial
+  Serial.println(currentMoisture);
+  Serial.println(currentLight);
+
+  //Sending Data to Firebase
+  Firebase.setFloat(fbdo, moisturePath, currentMoisture);
+  Firebase.setFloat(fbdo, lightPath, currentLight);
   
   //Delaying Restart
   delay(1000);
